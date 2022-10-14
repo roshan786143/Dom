ab20325b6cfc3c12e07b5e9e797bd02540edeee7
roshan786143 <roshanbabu.kng@gmail.com>
2022-10-15 01:04:08 +0530

    added some more js code

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-let items = document.getElementsByClassName("list-group-item");
-items[2].style.backgroundColor = "green";
-console.log(items[2]);
-for(let i=0;i<items.length;i++){
+let second_item = document.querySelector(".list-group-item:nth-child(2)");
 
-    items[i].style.fontWeight = "bold";
-  
-  }
+second_item.style.backgroundColor = "green";
+
+let second_item = document.querySelector(".list-group-item:nth-child(2)");
+
+second_item.style.display = "hidden";
+
+let second = document.querySelectorAll("li:nth-child(odd)");
+
+second[1].style.fontColor = "blue";
+
+let odd = document.querySelectorAll("li:nth-child(odd)");
+
+for (let i = 0; i < odd.length; i++) {
+  odd[i].style.backgroundColor = "green";
+}
+
+//The difference between these two is that querySelector returns a single element matching its selection criteria. querySelectorAll, on the other hand, returns all elements matching the search criteria in an iterable list.
