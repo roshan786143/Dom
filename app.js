f9dd425ba70f31e17a0a3472415116c92a02c73a
roshan786143 <roshanbabu.kng@gmail.com>
2022-10-14 23:48:01 +0530

    created a new js file

---
+++ app.js
@@ -0,0 +1,8 @@
+let items = document.getElementsByClassName("list-group-item");
+items[2].style.backgroundColor = "green";
+console.log(items[2]);
+for(let i=0;i<items.length;i++){
+
+    items[i].style.fontWeight = "bold";
+  
+  }
